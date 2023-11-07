1edcb740318743170ccf18d6264e502289a4e799
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 18:16:40 +0530

    reformatted

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,17 +1,14 @@
-import mongoose from "mongoose";
-
-export let DB: typeof mongoose | null = null;
+import mongoose from 'mongoose';
 
 export function connectToMongodb(callback: Function) {
-    mongoose.connect(process.env.MONGO_URI!)
-        .then((client) => {
-            console.log('[+] DB CONNECTED SUCCESSFULLY');
-            DB = client;
-            callback();
-        })
-        .catch((error) => {
-            console.log('[-] DB CONNECTION FAILED');
-            console.log(error)
-        })
+  mongoose
+    .connect(process.env.MONGO_URI!)
+    .then((client) => {
+      console.log('[+] DB CONNECTED SUCCESSFULLY');
+      callback();
+    })
+    .catch((error) => {
+      console.log('[-] DB CONNECTION FAILED');
+      console.log(error);
+    });
 }
-
