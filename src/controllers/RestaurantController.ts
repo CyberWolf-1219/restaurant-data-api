09179cc81e5b81831a376efd4d17db40f4f85fd0
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 18:12:37 +0530

    implemented remove endpoint

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,68 +1,85 @@
 import { Handler } from 'express';
-import Restaurant from '../models/Restaurant'
+import Restaurant from '../models/Restaurant';
 
 export const createRestaurant: Handler = async (req, res, next) => {
-  console.log("RESTAURANT CREATE ".padEnd(80, "="));
+  console.log('RESTAURANT CREATE '.padEnd(80, '='));
   try {
-    const newRestaurantData = req.body
+    const newRestaurantData = req.body;
     const newRestaurant = new Restaurant(newRestaurantData);
-    const validationResult = await newRestaurant.validate()
-    console.log(validationResult)
+    const validationResult = await newRestaurant.validate();
+    console.log(validationResult);
     const saveResult = await newRestaurant.save();
-    console.log(saveResult)
-    res.status(200).json({ message: 'OK' })
+    console.log(saveResult);
+    res.status(200).json({ message: 'OK' });
   } catch (error) {
-    res.status(500).json({ message: error })
+    res.status(500).json({ message: error });
   }
-  console.log("".padEnd(80, "="))
+  console.log(''.padEnd(80, '='));
 };
 
 export const getAllRestaurants: Handler = async (req, res, next) => {
-  console.log("RESTAURANT INDEX ".padEnd(80, "="))
+  console.log('RESTAURANT INDEX '.padEnd(80, '='));
   try {
     const result = await Restaurant.find();
-    console.log(result)
+    console.log(result);
     res.status(200).json({ message: result });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: error })
+    console.log(error);
+    res.status(500).json({ message: error });
   }
-  console.log("".padEnd(80, "="))
+  console.log(''.padEnd(80, '='));
 };
 
 export const viewRestaurant: Handler = async (req, res, next) => {
-  console.log("RESTAURANT VIEW ".padEnd(80, "="))
+  console.log('RESTAURANT VIEW '.padEnd(80, '='));
   try {
     const params = req.params;
     if (!params.restaurant_id) {
-      throw new Error('[-] NO RESTAURANT ID WAS SENT')
+      throw new Error('[-] NO RESTAURANT ID WAS SENT');
     }
 
     const queryResult = await Restaurant.findById(params.restaurant_id);
-    console.log(queryResult)
+    console.log(queryResult);
     res.status(200).json({ message: queryResult });
   } catch (error) {
-    res.status(500).json({ messag: error })
+    res.status(500).json({ messag: error });
   }
-  console.log("".padEnd(80, "="))
+  console.log(''.padEnd(80, '='));
 };
 
 export const updateRestaurant: Handler = async (req, res, next) => {
-  console.log("RESTAURANT VIEW ".padEnd(80, "="))
+  console.log('RESTAURANT UPDATE '.padEnd(80, '='));
   try {
     const updateData = req.body;
     console.log(updateData);
-    const updateResult = await Restaurant.updateOne({ _id: updateData._id }, updateData);
-    console.log(updateResult)
+    const updateResult = await Restaurant.updateOne(
+      { _id: updateData._id },
+      updateData
+    );
+    console.log(updateResult);
     res.status(200).json({ message: updateResult });
   } catch (error) {
     res.status(500).json({ message: error });
   }
 
-  console.log("".padEnd(80, "="))
+  console.log(''.padEnd(80, '='));
 };
 
-export const removeRestaurant: Handler = (req, res, next) => {
-  console.log(req.url);
-  res.status(200).json({ message: 'REMOVED' });
+export const removeRestaurant: Handler = async (req, res, next) => {
+  console.log('RESTAURANT REMOVE '.padEnd(80, '='));
+  try {
+    const id = req.params.restaurant_id;
+    console.log(id);
+    if (!id) {
+      throw new Error('[-] NO RESTAURANT ID WAS PROVIDED');
+    }
+
+    const removeResult = await Restaurant.deleteOne({ _id: id });
+    console.log(removeResult);
+    res.status(200).json({ message: 'OK' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+  console.log(''.padEnd(80, '='));
 };
