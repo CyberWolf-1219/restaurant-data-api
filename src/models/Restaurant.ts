1dc62ad6c5caae362dd2a856412aa05b9737a22e
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 22:35:48 +0530

    fixed typos

--- src/models/Restaurant.ts
+++ src/models/Restaurant.ts
@@ -1,97 +1,96 @@
-import mongoose, { Document, Schema, } from "mongoose";
+import mongoose, { Document, Schema } from 'mongoose';
 
 interface IRestaurant extends Document {
-    name: string;
-    address: {
-        street: string;
-        city: string;
-        country: string;
-    };
-    contact: {
-        phone: string;
-        email: string;
-    };
-    ratings:
-    {
-        userID: string;
-        rating: number;
-    }[],
+  name: string;
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  contacts: {
+    phone: string;
+    email: string;
+  };
+  ratings: {
+    userID: string;
     rating: number;
-    menu: {
-        name: string;
-        price: number
-    }[],
-    openHours:
-    {
-        day: "MON" | "TUE" | "WED" | "THIR" | "FIR" | "SAT" | "SUN"
-        open: string;
-        close: string
-    }[],
+  }[];
+  rating: number;
+  menu: {
+    name: string;
+    price: number;
+  }[];
+  openHours: {
+    day: 'MON' | 'TUE' | 'WED' | 'THIR' | 'FIR' | 'SAT' | 'SUN';
+    opensAt: string;
+    closesAt: string;
+  }[];
 }
 
-
-const RestaurantSchema: Schema = new Schema(
+const RestaurantSchema: Schema<IRestaurant> = new Schema({
+  name: { type: String, required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  contacts: {
+    phone: {
+      type: String,
+      validate: (value: string) => {
+        return /\+?\d{1,4} \d{3} \d{4}/.test(value);
+      },
+      required: true,
+    },
+    email: {
+      type: String,
+      validate: (value: string) => {
+        return /^[^\s@]+\@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      required: true,
+    },
+  },
+  ratings: [
     {
-        name: { type: String, required: true },
-        address: {
-            street: { type: String, required: true },
-            city: { type: String, required: true },
-            country: { type: String, required: true },
+      userID: { type: String, required: true },
+      rating: {
+        type: Number,
+        min: [0, 'Invalid Rating'],
+        max: [5, 'Invalid Rating'],
+        required: true,
+      },
+    },
+  ],
+  rating: { type: Number, required: true },
+  menu: [
+    {
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+  ],
+  openHours: [
+    {
+      day: {
+        type: String,
+        enum: ['MON', 'TUE', 'WED', 'THI', 'FRI', 'SAT', 'SUN'],
+        required: true,
+      },
+      opensAt: {
+        type: String,
+        validate: (value: string) => {
+          return /\d\d:\d\d\s['AM'|'PM']/.test(value);
         },
-        contacts: {
-            phone: {
-                type: String,
-                validate: (value: string) => {
-                    return /\+?\d{1,4} \d{3} \d{4}/.test(value)
-                },
-                required: true,
-            },
-            email: {
-                type: String,
-                validate: (value: string) => {
-                    return /^[^\s@]+\@[^\s@]+\.[^\s@]+$/.test(value)
-                },
-                required: true,
-            }
+        required: true,
+      },
+      closesAt: {
+        type: String,
+        validate: (value: string) => {
+          return /\d\d:\d\d\s['AM'|'PM']/.test(value);
         },
-        ratings: [
-            {
-                userID: { type: String, required: true },
-                rating: {
-                    type: Number,
-                    min: [ 0, "Invalid Rating" ],
-                    max: [ 5, "Invalid Rating" ],
-                    required: true,
-                },
-            } ],
-        rating: { type: Number, required: true },
-        menu: [ {
-            name: { type: String, required: true },
-            price: { type: Number, required: true }
-        } ],
-        openHours:
-            [ {
-                day: {
-                    type: String,
-                    enum: [ 'MON', 'TUE', 'WED', 'THI', 'FRI', 'SAT', 'SUN' ],
-                    required: true,
-                },
-                opensAt: {
-                    type: String,
-                    validate: (value: string) => {
-                        return /\d\d:\d\d\s['AM'|'PM']/.test(value);
-                    },
-                    required: true
-                },
-                closesAt: {
-                    type: String,
-                    validate: (value: string) => {
-                        return /\d\d:\d\d\s['AM'|'PM']/.test(value);
-                    },
-                    required: true,
-                }
-            } ],
-    }
-);
+        required: true,
+      },
+    },
+  ],
+});
 
 export default mongoose.model<IRestaurant>('Restaurant', RestaurantSchema);
