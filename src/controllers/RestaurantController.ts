5c4ca46c3fb546724ac9611707b3e0796ae3d368
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 16:47:05 +0530

    implemented index & create endpoints

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,26 +1,46 @@
 import { Handler } from 'express';
+import Restaurant from '../models/Restaurant'
 
-export const createRestaurant: Handler = (req, res, next) => {
-  console.log(req.url);
-  res.status(200).send('ALL ENTRIES');
+export const createRestaurant: Handler = async (req, res, next) => {
+  console.log("RESTAURANT CREATE ".padEnd(80, "="));
+  try {
+    const newRestaurantData = req.body
+    const newRestaurant = new Restaurant(newRestaurantData);
+    const validationResult = await newRestaurant.validate()
+    console.log(validationResult)
+    const saveResult = await newRestaurant.save();
+    console.log(saveResult)
+    res.status(200).json({ message: 'OK' })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+  console.log("".padEnd(80, "="))
 };
 
-export const getAllRestaurants: Handler = (req, res, next) => {
-  console.log(req.url);
-  res.status(200).send('ALL ENTRIES');
+export const getAllRestaurants: Handler = async (req, res, next) => {
+  console.log("RESTAURANT INDEX ".padEnd(80, "="))
+  try {
+    const result = await Restaurant.find();
+    console.log(result)
+    res.status(200).json({ message: result });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: error })
+  }
+  console.log("".padEnd(80, "="))
 };
 
 export const viewRestaurant: Handler = (req, res, next) => {
   console.log(req.params);
-  res.status(200).send('ONE ENTRY');
+  res.status(200).json({ message: 'ONE ENTRY' });
 };
 
 export const updateRestaurant: Handler = (req, res, next) => {
   console.log(req.url);
-  res.status(200).send('ALL');
+  res.status(200).json({ message: 'UPDATED' });
 };
 
 export const removeRestaurant: Handler = (req, res, next) => {
   console.log(req.url);
-  res.status(200).send('ALL');
+  res.status(200).json({ message: 'REMOVED' });
 };
